4c1f3e564a64becddca64fb378df747e1ad5cfb1
Zayne <star1582@naver.com>
2025-05-12 22:01:47 +0900

    DAO 생성

---
+++ BoardMVC/src/main/java/model/BoardDAO.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BoardDAO {
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	public void getCon() {
+		
+	}
+}
